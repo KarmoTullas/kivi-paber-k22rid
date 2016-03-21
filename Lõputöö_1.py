7c1a2d9753bb3dc779c6f3af3aa88238f1ca8db7
KarmoTullas <tarmokullas@gmail.com>
2016-03-21 12:25:00 +0200

    Peaegu

--- Lõputöö_1.py
+++ Lõputöö_1.py
@@ -2,7 +2,7 @@
 #Kivi-paber-käärid
 #Koostasid Tarmo Kullas & Tarvo Mäesepp
 
-import pygame, random, sys, time
+import pygame, random, time, sys
 
 #Ekraan
 pygame.init()
@@ -12,17 +12,29 @@ ekraan.fill([246, 231, 195])
 #Esileht
 header = pygame.image.load("header.png") #493x71 px
 ekraan.blit(header, (73.5, 60))
-
 alusta = pygame.image.load("alusta.png") #260x61 px
 ekraan.blit(alusta, (190, 260))
-
 pygame.display.flip()
+
+def valimine(n):
+    if i.type == pygame.MOUSEBUTTONDOWN:
+                        mx, my = pygame.mouse.get_pos()
+                        if mx > 70 and mx < 198 and my > (n*147.2)-128 and my < n*147.2:
+                            e = False
+                            if n == 1:
+                                valik = "kivi"
+                            if n == 2:
+                                valik = "paber"
+                            if n == 3:
+                                valik = "käärid"
+                            return e
+                            return valik
             
 while True:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             sys.exit()
-        elif i.type == pygame.MOUSEBUTTONDOWN and i.button == 1:
+        elif i.type == pygame.MOUSEBUTTONDOWN:
             mx, my = pygame.mouse.get_pos()
             if mx > 190 and mx < 450 and my > 260 and my < 321:
 
@@ -39,21 +51,59 @@ while True:
                 ekraan.blit(käärid, (70, 313.6))
 
                 pygame.display.flip()
+    
+                e = True
+                while e == True:
+                    for k in pygame.event.get():
+                        x = random.randint(0, 3)
+                        if x == 0:
+                            pygame.draw.rect(ekraan, [246, 231, 195], [442, 166.4, 128, 128], 0)
+                            ekraan.blit(kivi, (442, 166.4))
+                            pygame.display.flip()
+                            muutuja = "kivi"
+                            time.sleep(0.05)
+                        elif x == 1:
+                            pygame.draw.rect(ekraan, [246, 231, 195], [442, 166.4, 128, 128], 0)
+                            ekraan.blit(paber, (442, 166.4))
+                            pygame.display.flip()
+                            muutuja = "paber"
+                            time.sleep(0.05)
+                        elif x == 2:
+                            pygame.draw.rect(ekraan, [246, 231, 195], [442, 166.4, 128, 128], 0)
+                            ekraan.blit(käärid, (442, 166.4))
+                            pygame.display.flip()
+                            muutuja = "käärid"
+                            time.sleep(0.05)
+                        if k.type == pygame.MOUSEBUTTONDOWN:
+                            mx, my = pygame.mouse.get_pos()
+                            if mx > 70 and mx < 198 and my > (1*147.2)-128 and my < 1*147.2:
+                                e = False
+                                valik = "kivi"
+                            if mx > 70 and mx < 198 and my > (2*147.2)-128 and my < 2*147.2:
+                                e = False
+                                valik = "paber"
+                            if mx > 70 and mx < 198 and my > (3*147.2)-128 and my < 3*147.2:
+                                e = False
+                                valik = "käärid"
+                        elif k.type == pygame.MOUSEBUTTONUP:
+                            e = True
+                  
+                if valik == muutuja:
+                    print ("viik")
+                elif valik == "kivi" and muutuja == "paber":
+                    print ("kaotus")
+                elif valik == "kivi" and muutuja == "käärid":
+                    print ("võit")
+                elif valik == "paber" and muutuja == "kivi":
+                    print ("võit")
+                elif valik == "paber" and muutuja == "käärid":
+                    print ("kaotus")
+                elif valik == "käärid" and muutuja == "kivi":
+                    print ("kaotus")
+                elif valik == "käärid" and muutuja == "paber":
+                    print ("võit")
+                        
+            
 
-                i = "õige"
-                while i == "õige":
-                    pygame.draw.rect(ekraan, [246, 231, 195], [442, 166.4, 128, 128], 0)
-                    ekraan.blit(kivi, (442, 166.4))
-                    pygame.display.flip()
-                    time.sleep(0.1)
-                    pygame.draw.rect(ekraan, [246, 231, 195], [442, 166.4, 128, 128], 0)
-                    ekraan.blit(paber, (442, 166.4))
-                    pygame.display.flip()
-                    time.sleep(0.1)
-                    pygame.draw.rect(ekraan, [246, 231, 195], [442, 166.4, 128, 128], 0)
-                    ekraan.blit(käärid, (442, 166.4))
-                    pygame.display.flip()
-                    time.sleep(0.1)
 
-Tarvo on pedeeee
                
